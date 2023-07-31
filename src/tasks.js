e663cab30369e3c93683ffdb6350adcb353f205e
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-07-31 13:18:03 +0300

    Feat task factory function

---
+++ src/tasks.js
@@ -0,0 +1,33 @@
+function tasksFactory(obj = {}) {
+    let title = '';
+    let description = '';
+    const today = new Date();
+    let dueDate = [today.getDay() + 1, today.getMonth, today.getFullYear];
+    let priority = 0;
+    
+    const setTitle = function(t) {
+        this.title = t;
+    };
+    
+    const setDescription = function(d) {
+        this.description = d;
+    };
+    
+    const setDueDate = function(dd) {
+        this.dueDate = dd;
+    };
+    
+    const setPriority = function(p) {
+        this.priority = p;
+    }
+    
+    return Object.assign(obj, {title, 
+        description,
+        dueDate,
+        priority,
+        setTitle,
+        setDescription,
+        setDueDate,
+        setPriority
+    });
+}
