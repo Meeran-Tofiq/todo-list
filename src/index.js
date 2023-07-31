2aa394eb7e91c6dd4f15770068ee8ae01a51fe0d
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-07-31 13:31:15 +0300

    Feat testing for tasks.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import tasksFactory from "./tasks";
+
 console.log('development');
+
+const t = tasksFactory();
+
+t.setTitle("Clean Shower");
+t.setDescription("I have to clean up my shower.");
+t.setPriority(5);
+console.log(t);
