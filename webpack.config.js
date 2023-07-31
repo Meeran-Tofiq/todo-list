3b3f95939af8e72d86116522b2ef7e171d99c865
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-07-31 12:42:03 +0300

    Add source mapping

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
+    devtool: 'inline-source-map',
     plguins: [
         new HtmlWebpackPlugin({
             title: 'ToDo List',
