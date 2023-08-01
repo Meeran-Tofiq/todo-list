e52a023b7a18f4b1763dce1b92dbe699f640a7c4
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-08-01 14:23:48 +0300

    Feat factory for projects

---
+++ src/projects.js
@@ -0,0 +1,27 @@
+function projectsFactory(obj = {}) {
+    let name = 'New Project';
+    const tasks = [];
+
+    const getName = () => {
+        return name;
+    };
+
+    const getTasks = () => {
+        return tasks;
+    };
+
+    const setName = (_name) => {
+        name = _name;
+    };
+
+    const addTask = (task) => {
+        tasks.push(task);
+    };
+
+    return Object.assign(obj, {
+        getName,
+        getTasks,
+        setName,
+        addTask
+    });
+}
