7942b607cbe793a0f112284706cf2be17ebd72b4
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-08-01 14:26:28 +0300

    Clean up unnecessary code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import "./all.min";
 import loadPage from "./load_page";
 loadPage();
+
+
 import tasksFactory from "./tasks";
 import './styles.css';
-
-console.log('development');
-
-const t = tasksFactory();
-
-t.setTitle("Clean Shower");
-t.setDescription("I have to clean up my shower.");
-t.setPriority(5);
-console.log(t);
